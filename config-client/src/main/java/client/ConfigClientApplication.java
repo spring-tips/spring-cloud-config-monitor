759542cd0fa52cd9102fc29e2f2abbf84712f477
Josh Long <josh@joshlong.com>
2021-10-06 02:27:43 -0700

    up

--- config-client/src/main/java/client/ConfigClientApplication.java
+++ config-client/src/main/java/client/ConfigClientApplication.java
@@ -15,10 +15,19 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 */
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.bus.event.RefreshRemoteApplicationEvent;
+import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class ConfigClientApplication {
@@ -33,19 +42,29 @@ public class ConfigClientApplication {
                 System.getenv().forEach((k, v) -> System.out.println(k + "=" + v));
     }
 }
-/*
 
-@Component
+@Controller
+@ResponseBody
 @RequiredArgsConstructor
 class PropertyListener {
 
+
     private final Environment environment;
+
+    @GetMapping("/value")
+    String read() {
+        return this.readValue();
+    }
 
     @EventListener({
             RefreshRemoteApplicationEvent.class,
             ApplicationReadyEvent.class,
             RefreshScopeRefreshedEvent.class})
     public void refresh() {
-        System.out.println("the new value is " + this.environment.getProperty("cnj.message"));
+        System.out.println("the new value is " + this.readValue());
     }
-}*/
+
+    private String readValue() {
+        return this.environment.getProperty("cnj.message");
+    }
+}
