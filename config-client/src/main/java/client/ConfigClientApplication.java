3e30f946ea762537e97a6af80b3f631bb6feed85
Josh Long <josh@joshlong.com>
2021-10-06 01:26:23 -0700

    up

--- config-client/src/main/java/client/ConfigClientApplication.java
+++ config-client/src/main/java/client/ConfigClientApplication.java
@@ -2,11 +2,13 @@ package client;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.bus.event.RefreshRemoteApplicationEvent;
 import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
@@ -15,9 +17,13 @@ import org.springframework.stereotype.Component;
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run( ConfigClientApplication.class, args);
+        SpringApplication.run(ConfigClientApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner runner() {
+        return args -> System.getenv().forEach((k, v) -> System.out.println(k + "=" + v));
+    }
 }
 
 @Component
